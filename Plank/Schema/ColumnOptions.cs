e34b0dff83682861a906d737d2b900341f2e89da
Kuinox <github@kuinox.io>
2026-02-03 11:34:19 +0100

    Remove ColumnOptions builder helpers

--- Plank/Schema/ColumnOptions.cs
+++ Plank/Schema/ColumnOptions.cs
@@ -5,19 +5,4 @@ namespace Plank.Schema;
 public readonly record struct ColumnOptions(ParquetRepetition Repetition, ImmutableArray<EncodingKind> Encodings)
 {
     public static readonly ColumnOptions Default = new(ParquetRepetition.Unspecified, ImmutableArray<EncodingKind>.Empty);
-
-    public ColumnOptions WithRepetition(ParquetRepetition repetition)
-        => new(repetition, NormalizedEncodings());
-
-    public ColumnOptions Optional()
-        => WithRepetition(ParquetRepetition.Optional);
-
-    public ColumnOptions Required()
-        => WithRepetition(ParquetRepetition.Required);
-
-    public ColumnOptions WithEncoding(EncodingKind encoding)
-        => new(Repetition, NormalizedEncodings().Add(encoding));
-
-    ImmutableArray<EncodingKind> NormalizedEncodings()
-        => Encodings.IsDefault ? ImmutableArray<EncodingKind>.Empty : Encodings;
 }
